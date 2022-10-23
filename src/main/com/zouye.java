a2aaf3b73ccc66c0a145c36cb23c650143975aa3
Xcaesar1 <89081227+Xcaesar1@users.noreply.github.com>
2022-10-23 20:34:39 +0800

    Update zouye.java

--- src/main/com/zouye.java
+++ src/main/com/zouye.java
@@ -20,6 +20,7 @@ public class zouye extends HttpServlet {
         list.add(new entity("5.jpeg", "王五", 23, "男"));
         list.add(new entity("6.jpeg", "赵六", 20, "男"));
         System.out.println("hello");
+            System.out.println("By github");
         req.setAttribute("list", list);
         req.getRequestDispatcher("zouye.jsp").forward(req, resp);
     }
