c240656cd479a3e91d5fe991e3b062d19026529a
Caesar <god3kill@163.com>
2022-10-23 20:30:25 +0800

    fourth Commit

--- src/main/com/zouye.java
+++ src/main/com/zouye.java
@@ -20,8 +20,6 @@ public class zouye extends HttpServlet {
         list.add(new entity("5.jpeg", "王五", 23, "男"));
         list.add(new entity("6.jpeg", "赵六", 20, "男"));
         System.out.println("hello");
-        System.out.println("master");
-        System.out.println("hot-fix");
         req.setAttribute("list", list);
         req.getRequestDispatcher("zouye.jsp").forward(req, resp);
     }
